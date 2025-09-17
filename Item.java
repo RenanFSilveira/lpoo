598bf2165a3917aa5ec46a1cf50853f1ee85e87b
RenanFSilveira <146729229+RenanFSilveira@users.noreply.github.com>
2025-09-17 08:26:43 -0300

    Add files via upload

---
+++ Item.java
@@ -0,0 +1,22 @@
+public class Item {
+    private int chave;
+
+    public int getChave() {
+        return chave;
+    }
+
+    public void setChave(int chave) {
+        this.chave = chave;
+    }
+
+
+    public Item(int chave) {
+        this.chave = chave;
+    }
+
+    @Override
+    public String toString() {
+        return "Item [chave=" + chave + "]";
+    }    
+
+}
